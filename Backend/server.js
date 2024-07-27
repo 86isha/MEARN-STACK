8f1f7ddf860109a68928c8f28c98e33a6b7efd3a
86isha <86isha.gupta@gmail.com>
2024-07-27 18:03:56 +0530

    new ass

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,42 @@
+//---monolithic architecture----//
+
+
+
+
+// const express = require("express");
+// const colors = require("colors");
+// const dotenv = require("dotenv").config();
+// const app = express();
+
+// const PORT = 5000;
+
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: "Hello World",
+//   });
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`Server running on port ${PORT}`.yellow.bold)
+// );
+
 const express = require("express");
 const colors = require("colors");
-const dotenv = require("dotenv").config();
-const app = express();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Hello World",
-  });
-});
+const app = express();
+//-----properties of express has been transfered into app------//
+app.use(express.json());
+//---Here express.json is middleware which is predefined(jo pehle se hi diya hua h)---//
 
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`.yellow.bold)
-);
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+    });
+    app.listen(PORT, () =>{
+        console.log(`Server running on port ${PORT}`.yellow.bold)
+      });
+      
--- NodeJS/new.js
+++ NodeJS/new.js
@@ -1,7 +1,7 @@
 const getdata = async () => {
     try{
-        const response = await fetch("'https://dummyjson.com/users'"); //add link to your jason data in brackets
-        datas = await response.jason(); //data is in object(binary) format ==>
+        const response = await fetch("https://dummyjson.com/users"); //add link to your jason data in brackets
+        datas = await response.json(); //data is in object(binary) format ==>
             console.log(datas);
     }catch(error){
         console.log(error);
